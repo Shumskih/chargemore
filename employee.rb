018ed5f5284bd7b21ee9acdb8cb917ea3c6d3379
Shumskih <music.dll@mail.ru>
2016-11-03 06:24:39 +0300

    Added superclass Employee and subclasses and method initialize with super keyword

--- employee.rb
+++ employee.rb
@@ -1,6 +1,6 @@
 class Employee
 
-  attr_reader :name, :salary
+  attr_reader :name
 
   def name=(name)
     if name == ""
@@ -9,6 +9,20 @@ class Employee
     @name = name
   end
 
+  def initialize(name = "Anonymous")
+    self.name = name
+  end
+
+  def print_name
+    puts "Name: #{name}"
+  end
+end
+
+
+
+class SalariedEmployee < Employee
+
+  attr_reader :salary
 
   def salary=(salary)
     if salary < 0
@@ -17,20 +31,57 @@ class Employee
     @salary = salary
   end
 
-  def initialize(name = "Anonymous", salary = "0.0")
-    self.name = name
+  def initialize(name = "Anonymous", salary = 0.0)
+    super(name)
     self.salary = salary
   end
 
   def print_pay_stub
-    puts "Name: #{@name}"
-    pay_for_period = (@salary / 365.0) * 14
-    formatted_pay = format("%.2f", pay_for_period)
-    puts "Pay this period: $#{formatted_pay}"
+    print_name
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay for period: #{formatted_pay}"
   end
 
 end
 
 
-amy = Employee.new("Amy Blake", 50000)
-amy.print_pay_stub
+class HourlyEmployee < Employee
+
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "A hourly_wage of #{hourly_wage} isn't valid!"
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "A hours_per_week of #{hours_per_week} isn't valid!"
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = (hourly_wage * hours_per_week) * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay for period: #{formatted_pay}"
+  end
+
+end
+
+
+salaied_employee = SalariedEmployee.new("Jane Doe", 50000)
+salaied_employee.print_pay_stub
+
+hourly_employee = HourlyEmployee.new("John Smith", 14.97, 30)
+hourly_employee.print_pay_stub
